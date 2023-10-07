0f5359d24dfa58138b887f74831d1a91ec532515
thedevangvishnu <thedevangvishnu@gmail.com>
2023-10-07 17:22:39 +0530

    Learn about setState() and make notes
    In react basics section of the course, watch videos on setState().
    Learn what it is and how it is used to update state inside a
    component. Also learn about shallow merge.
    
    Make notes on the same in notes/2-react-basics.

--- ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
+++ ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Devang",
+      name: { firstName: "Devang", lastName: "Vishnu" },
+      company: "ZTM",
     };
   }
 
@@ -17,8 +18,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Change name</button>
+          <p>
+            Hello, I am {this.state.name.firstName} {this.state.name.lastName}{" "}
+            and I work at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  console.log("updater function:", this.state); // before update
+                  return { name: { firstName: "Amika", lastName: "khatri" } };
+                },
+                () => {
+                  console.log("callback function: ", this.state); // after update
+                }
+              );
+              // console.log(this.state);
+            }}
+          >
+            Change name
+          </button>
         </header>
       </div>
     );
