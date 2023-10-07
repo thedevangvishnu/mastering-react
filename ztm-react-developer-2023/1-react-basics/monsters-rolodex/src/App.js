5756df509bd524b9ea844986919c7c059ba3c583
thedevangvishnu <thedevangvishnu@gmail.com>
2023-10-07 17:33:29 +0530

    Create a new React app monsters-rolodex
    Create a new React app in 1-react-basics for a project named
    monsters-rolodex which would allow to put all that has been learned
    into making a simple one-page React app with real-time search
    feature.

--- ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
+++ ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
@@ -1,47 +1,25 @@
-import { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  // adding a constructor()
-  constructor() {
-    super();
-
-    this.state = {
-      name: { firstName: "Devang", lastName: "Vishnu" },
-      company: "ZTM",
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, I am {this.state.name.firstName} {this.state.name.lastName}{" "}
-            and I work at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  console.log("updater function:", this.state); // before update
-                  return { name: { firstName: "Amika", lastName: "khatri" } };
-                },
-                () => {
-                  console.log("callback function: ", this.state); // after update
-                }
-              );
-              // console.log(this.state);
-            }}
-          >
-            Change name
-          </button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
