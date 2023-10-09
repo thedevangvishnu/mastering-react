4c8a6e9a3e4fb77284f6aa9cf855556420ebbe3f
thedevangvishnu <thedevangvishnu@gmail.com>
2023-10-09 19:45:39 +0530

    Refactor App.js by replacing class component vs functional component

--- ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
+++ ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/App.js
@@ -1,62 +1,116 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/CardList";
 import SearchBox from "./components/search-box/SearchBox";
 
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  console.log("render");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [searchField, setSearchField] = useState("");
+  const [searchString, setSearchString] = useState("");
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+  // fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then((response) => response.json())
+  //   .then((users) => setMonsters(users));
+  // here, an infinite render loop is being executed fetch is storing user in different location is memory, which let React update state, even though it gives us the same array, reference to that array is different. Hence, the state gets updated, which triggers re-render, which then run fetch() again and again the state gets updated. Hence, creating a render loop
 
-  componentDidMount() {
+  useEffect(() => {
+    console.log("effect fired");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchString = event.target.value.toLocaleLowerCase();
-    this.setState({ searchField: searchString });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="app-title">Monsters Rolodex</h1>
-          <SearchBox
-            className="monsters-search-box"
-            onChangeHandler={onSearchChange}
-            placeholderText="search monster"
-          />
-          <CardList monsters={filteredMonsters} />
-        </header>
-      </div>
-    );
-  }
-}
+  const onSearchChange = (event) => {
+    const searchString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchString);
+  };
+
+  const onSearchStringChange = (event) => {
+    setSearchString(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          className="monsters-search-box"
+          onChangeHandler={onSearchChange}
+          placeholderText="search monster"
+        />
+        <SearchBox
+          onChangeHandler={onSearchStringChange}
+          placeholderText="search string"
+        />
+        <CardList monsters={filteredMonsters} />
+      </header>
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           () => {
+//             console.log(this.state);
+//           }
+//         )
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchString = event.target.value.toLocaleLowerCase();
+//     this.setState({ searchField: searchString });
+//   };
+
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="app-title">Monsters Rolodex</h1>
+//           <SearchBox
+//             className="monsters-search-box"
+//             onChangeHandler={onSearchChange}
+//             placeholderText="search monster"
+//           />
+//           <CardList monsters={filteredMonsters} />
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/components/card/Card.js
+++ ztm-react-developer-2023/1-react-basics/monsters-rolodex/src/components/card/Card.js
@@ -5,7 +5,7 @@ class Card extends Component {
   render() {
     const { name, email, id } = this.props.monster;
     return (
-      <div key={id} className="card-container">
+      <div className="card-container">
         <img
           src={`https://robohash.org/${id}?set=set2&size=200x200`}
           alt={name}
