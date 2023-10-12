00f035e7d0224085ecbf7f9bdc75490bd2d074a4
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-12 14:25:17 +0530

    Install sass and apply styles using sass to App.js
    Using npm, install sass, and create a new file to write styles for
    categories component.

--- ztm-react-developer-2023/crown-clothing/src/App.js
+++ ztm-react-developer-2023/crown-clothing/src/App.js
@@ -1,55 +1,42 @@
+import "./categories-style.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div className="categories__container">
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2></h2>
-          <p>Show now</p>
+    <div className="categories-container">
+      {categories.map(({ id, title }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Show now</p>
+          </div>
         </div>
-      </div>
-
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2>Hats</h2>
-          <p>Show now</p>
-        </div>
-      </div>
-
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2>Jackets</h2>
-          <p>Show now</p>
-        </div>
-      </div>
-
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2>Sneakers</h2>
-          <p>Show now</p>
-        </div>
-      </div>
-
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2>Women</h2>
-          <p>Show now</p>
-        </div>
-      </div>
-
-      <div className="category__container">
-        {/* <img src="" alt="" /> */}
-        <div className="category__body__container">
-          <h2>Men</h2>
-          <p>Show now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
