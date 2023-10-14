bdaf6231f1b7742ef3531a89336fb44e3f0082d0
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-14 11:56:09 +0530

    Set up authentication and firestore
    Complete the initial set up of firebase, specifically for
    authentication and firestore.
    
    Setup two buttons in the sign up page to verify that both
    authentication and firestore are working as expected.

--- ztm-react-developer-2023/crown-clothing/src/routes/sign-in/sign-in.component.jsx
+++ ztm-react-developer-2023/crown-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,38 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+        console.log("userDoc:", userDocRef);
+      }
+    };
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    console.log("user", user);
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log("userDoc:", userDocRef);
   };
+
   return (
     <div>
       <h1>I am the Sign In page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
--- ztm-react-developer-2023/crown-clothing/src/utils/firebase/firebase.utils.js
+++ ztm-react-developer-2023/crown-clothing/src/utils/firebase/firebase.utils.js
@@ -1,3 +1,5 @@
+/* 1 - Setting up firebase */
+
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -5,7 +7,9 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAw9aAPiyuVLWIRzBQTFg8r2zwwZexU6YM",
   authDomain: "crown-clothing-db-e6138.firebaseapp.com",
@@ -15,14 +19,85 @@ const firebaseConfig = {
   appId: "1:451600980806:web:6192d02ed0428eb904e347",
 };
 
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-// set up a provider
-const provider = new GoogleAuthProvider();
+/* ..................................................... */
 
-provider.setCustomParameters({
+/* 2 - Setting up authentication*/
+
+// create a new provider instance and set custom parameters
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
+// call getAuth() to store authentication
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// create a functionality for signing in with popup
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+// create a functionality for singing in with redirect
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+/* ..................................................... */
+
+/* 3 - Setting up Firestore database */
+
+export const db = getFirestore();
+
+// create a function that creates new user document from authetication
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "user", userAuth.uid);
+  const userSnapShot = await getDoc(userDocRef);
+  console.log("userSnapShot", userSnapShot.exists());
+
+  // create user document if it doesn't exist
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user:", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Notes on Firestore
+
+/* Firestore 
+
+1) Setting up firestore
+    - Sign in with google email into Firestore web app
+    - Create a data-base and name it to get the firebase config
+    - npm install firebase
+    - create a js file for firebase and import {initializeApp} from "firebase/app"
+    - Firebase has multiple sublibraries that have their own purpose.
+    - Create an instance of initializeApp() by passing the firebase config to it.
+
+2) Setting up Authentication
+    - Go to Firebase and select Authentication. 
+    - Select Google, Facebook or any other auth services of your choice.
+    - Enable it.
+    - Import authentication methods using "firebase/auth" library.
+    - getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider are some of the important ones
+    - Set up a new provider instance of GoogleAuthProvider() class and call setCustomParameters() method of provider instance to set custom parameter, which takes in an object (such as prompt: "select_account").
+    - Call getAuth() and store its reference.
+    - Create a function (such as SignInWithGoogle) using signInWithPopup() method from firebase/auth library. signInWithPopup() takes two parameters: auth and provider. Export this new functionality and call it on your event handler for the sign-in button.
+
+3) Setting up Firestore Database
+
+*/
