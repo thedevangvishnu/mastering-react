b42fb60402f2db2bc30548cbea111aeab40a29a8
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-25 12:36:50 +0530

    Style the basic outlook of checkout component

--- ztm-react-developer-2023/crown-clothing/src/components/checkout/checkout.component.jsx
+++ ztm-react-developer-2023/crown-clothing/src/components/checkout/checkout.component.jsx
@@ -7,22 +7,41 @@ const Checkout = () => {
     useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the Checkout Page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <p>{quantity}</p>
-              <p onClick={() => removeItemFromCart(cartItem)}>decrement</p>
-              <p onClick={() => addItemToCart(cartItem)}>increment</p>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
-      <h2>{`Total price is: $${totalPrice}`}</h2>
+
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <p>{quantity}</p>
+            <p onClick={() => removeItemFromCart(cartItem)}>decrement</p>
+            <p onClick={() => addItemToCart(cartItem)}>increment</p>
+          </div>
+        );
+      })}
+      <span className="total">{`Total: $${totalPrice}`}</span>
     </div>
   );
 };
