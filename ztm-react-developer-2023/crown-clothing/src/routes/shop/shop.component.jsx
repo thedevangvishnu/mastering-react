43cf9e409aabcec9f4ed6c369db76bf7db28ab30
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-26 14:50:17 +0530

    Implement nested routing in shop component
    Create a new category component route that displays all products of
    a particular category. Implement nested routing in shop page, so that
    each category is routed after shop page is routed.

--- ztm-react-developer-2023/crown-clothing/src/routes/shop/shop.component.jsx
+++ ztm-react-developer-2023/crown-clothing/src/routes/shop/shop.component.jsx
@@ -1,20 +1,14 @@
-import { useContext, Fragment } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import "./shop.styles.scss";
+import { Routes, Route } from "react-router-dom";
+
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
