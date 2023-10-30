8808c2f1cebf76557b547bdae664393fdd713972
Devang Vishnu <thedevangvishnu@gmail.com>
2023-10-30 19:23:39 +0530

    Delete unnecessary files and setup App using tailwind
    Add custom fonts and colors to tailwind.config.js using
    the style-guide.md and deelete unnecessary files.
    
    Clean up index.html

--- react-practice-projects/newbie-level/project-01/tailwind.config.js
+++ react-practice-projects/newbie-level/project-01/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "Kumbh Sans, sans-serif",
+      },
+      colors: {
+        darkCyan: "hsl(185, 75%, 39%)",
+        desaturatedBlue: "hsl(229, 23%, 23%)",
+        grayishBlue: "hsl(227, 10%, 46%)",
+        darkGray: "hsl(0, 0%, 59%)",
+      },
+    },
   },
   plugins: [],
 };
