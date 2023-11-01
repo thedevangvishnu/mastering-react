2c6fe55e6e6fbf627c7c36a58339b8e38eb317ac
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-01 10:11:56 +0530

    Clean up and set up project with necessary changes
    Delete unnecessary files from src folder. Clean up comments from
    index.html and clean up App.js.
    
    Configure tailwindcss config file to use custom styling stated in
    styles-guide.md. Add custom screens, fonts and colors.
    
    Add link to google fonts in index.html and test everything in App.js.

--- react-practice-projects/newbie-level/project-02/tailwind.config.js
+++ react-practice-projects/newbie-level/project-02/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "Red Hat Display, sans-serif",
+      },
+      screens: {
+        xs: { min: "375px", max: "640px" },
+      },
+      colors: {
+        paleBlue: "hsl(225, 100%, 94%)",
+        brightBlue: "hsl(245, 75%, 52%)",
+        veryPaleBlue: "hsl(225, 100%, 98%)",
+        desaturatedBlue: "hsl(224, 23%, 55%)",
+        darkBlue: "hsl(223, 47%, 23%)",
+      },
+    },
   },
   plugins: [],
 };
