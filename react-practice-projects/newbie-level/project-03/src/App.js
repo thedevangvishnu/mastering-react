e7451189c52270f0f42bb0f8cdef7765e3bebdb3
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-04 12:16:59 +0530

    Create the basic layout of background and container

--- react-practice-projects/newbie-level/project-03/src/App.js
+++ react-practice-projects/newbie-level/project-03/src/App.js
@@ -1,5 +1,17 @@
 const App = () => {
-  return <h1>Result summary component</h1>;
+  return (
+    // background
+    <div className="bg-slate-200 w-ful h-screen flex items-center justify-center">
+      {/* container div */}
+      <div className="w-1/3 h-3/5 bg-white rounded-2xl relative shadow-2xl flex">
+        {/* left seciton */}
+        <div className="w-1/2 h-full bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue rounded-2xl"></div>
+
+        {/* right section */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
