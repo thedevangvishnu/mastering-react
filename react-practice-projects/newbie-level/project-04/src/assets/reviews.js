dbb9e056a023f2fffa58f3b5b3cd97e8da54f9a0
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-07 13:34:24 +0530

    Create a reviews.js file to add all reviews

---
+++ react-practice-projects/newbie-level/project-04/src/assets/reviews.js
@@ -0,0 +1,27 @@
+import coltonImage from "./images/image-colton.jpg";
+import ireneImage from "./images/image-irene.jpg";
+import anneImage from "./images/image-anne.jpg";
+
+export const reviews = [
+  {
+    id: 1,
+    name: "Colton Smith",
+    image: coltonImage,
+    review:
+      "We need the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!",
+  },
+  {
+    id: 2,
+    name: "Irene Roberts",
+    image: ireneImage,
+    review:
+      "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
+  },
+  {
+    id: 3,
+    name: "Anne Wallace",
+    image: anneImage,
+    review:
+      "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
+  },
+];
