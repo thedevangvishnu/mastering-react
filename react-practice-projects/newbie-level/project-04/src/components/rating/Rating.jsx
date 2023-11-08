0043fa65e3d2875781d96f77cb16f3e065aab6f9
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-08 11:59:34 +0530

    Leverage useState and useEffect to apply styles based on minWidth
    The rating component have a different left-margin when they are viewed
    on medium and larger screen devices. Hence, use useState() and
    useEffect() to re-render Rating component to apply different styling
    based on screen sizes.

--- react-practice-projects/newbie-level/project-04/src/components/rating/Rating.jsx
+++ react-practice-projects/newbie-level/project-04/src/components/rating/Rating.jsx
@@ -1,20 +1,35 @@
 import star from "../../assets/images/icon-star.svg";
+import { useState, useEffect } from "react";
 
-const Rating = ({ margin, minWidth }) => {
-  let marginStyles;
+const defaultMargin = { marginLeft: "0px" };
 
-  if (window.innerWidth >= 768) {
-    marginStyles = { marginLeft: `${margin}px` };
-  } else {
-    marginStyles = { marginLeft: "0px" };
-  }
+const Rating = ({ margin }) => {
+  const [marginLeft, setMarginLeft] = useState(defaultMargin);
 
-  //   const marginStyles = margin !== undefined ? `ml-${margin}` : "";
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth >= 768) {
+        setMarginLeft({ marginLeft: `${margin}px` });
+      } else {
+        setMarginLeft(defaultMargin);
+      }
+    };
+
+    // Add a window resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [margin]);
 
   return (
     <div
       className={`bg-magenta-100 xs:flex-col flex flex-row items-center xs:p-4 p-3 relative xs:gap-2 gap-8 xs:w-full w-3/4 rounded-lg`}
-      style={marginStyles}
+      style={marginLeft}
     >
       <div className="flex items-center gap-1 h-full">
         {Array(5)
