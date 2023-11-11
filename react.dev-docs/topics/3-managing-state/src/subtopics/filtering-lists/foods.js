3370bf45e2b8c0444b11416a8240ced7a2dfaa94
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-11 20:43:45 +0530

    Create a new folder "filtering list" to learn the same
    Practice through code and learn how to filter a list.
    Create a foods.js file that contains list a food items.

---
+++ react.dev-docs/topics/3-managing-state/src/subtopics/filtering-lists/foods.js
@@ -0,0 +1,42 @@
+export const foods = [
+  {
+    id: 0,
+    name: "Sushi",
+    imageUrl:
+      "https://images.pexels.com/photos/8743914/pexels-photo-8743914.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "Sushi is a traditional Japanese dish of prepared vinegared rice",
+  },
+  {
+    id: 1,
+    name: "Dal",
+    imageUrl:
+      "https://st4.depositphotos.com/3823309/37970/i/380/depositphotos_379709004-stock-photo-dal-makhani-dark-background-dal.jpg",
+    description:
+      "The most common way of preparing dal is in the form of a soup to which onions, tomatoes and various spices may be added",
+  },
+  {
+    id: 2,
+    name: "Pierogi",
+    imageUrl:
+      "https://st3.depositphotos.com/3539137/17619/i/380/depositphotos_176195352-stock-photo-christmas-dumplings-decoration-grey-board.jpg",
+    description:
+      "Pierogi are filled dumplings made by wrapping unleavened dough around a savoury or sweet filling and cooking in boiling water",
+  },
+  {
+    id: 3,
+    name: "Shish kebab",
+    imageUrl:
+      "https://st4.depositphotos.com/1704325/26754/i/380/depositphotos_267546994-stock-photo-skewers-of-grilled-vegetables.jpg",
+    description:
+      "Shish kebab is a popular meal of skewered and grilled cubes of meat.",
+  },
+  {
+    id: 4,
+    name: "Dim sum",
+    imageUrl:
+      "https://st4.depositphotos.com/16102236/24140/i/380/depositphotos_241403440-stock-photo-traditional-chinese-steamed-dumplings-dim.jpg",
+    description:
+      "Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch",
+  },
+];
