be7a19f7b75af487a9cda980a4a9cee007efdfac
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-12 23:50:25 +0530

    Add more food items in the food.js list

--- react.dev-docs/topics/3-managing-state/src/subtopics/3-sharing-state-between-components/3-filtering-list/foods.js
+++ react.dev-docs/topics/3-managing-state/src/subtopics/3-sharing-state-between-components/3-filtering-list/foods.js
@@ -5,7 +5,7 @@ export const foods = [
     imageUrl:
       "https://images.pexels.com/photos/8743914/pexels-photo-8743914.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
-      "Sushi is a traditional Japanese dish of prepared vinegared rice",
+      "Sushi is a traditional Japanese dish of prepared vinegared rice.",
   },
   {
     id: 1,
@@ -13,7 +13,7 @@ export const foods = [
     imageUrl:
       "https://st2.depositphotos.com/5653638/11431/i/380/depositphotos_114311142-stock-photo-basmati-rice-slowly-cooked-with.jpg",
     description:
-      "Biryani is a mixed rice dish originating among the Muslims of South Asia. It is made with rice, some type of meat and spices.",
+      "Biryani is a mixed rice dish originating among the Muslims of South Asia. It is made with rice, some type of meat, and spices.",
   },
   {
     id: 2,
@@ -21,7 +21,7 @@ export const foods = [
     imageUrl:
       "https://st3.depositphotos.com/3539137/17619/i/380/depositphotos_176195352-stock-photo-christmas-dumplings-decoration-grey-board.jpg",
     description:
-      "Pierogi are filled dumplings made by wrapping unleavened dough around a savoury or sweet filling and cooking in boiling water",
+      "Pierogi are filled dumplings made by wrapping unleavened dough around a savory or sweet filling and cooking in boiling water.",
   },
   {
     id: 3,
@@ -37,6 +37,47 @@ export const foods = [
     imageUrl:
       "https://st4.depositphotos.com/16102236/24140/i/380/depositphotos_241403440-stock-photo-traditional-chinese-steamed-dumplings-dim.jpg",
     description:
-      "Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch",
+      "Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch.",
+  },
+  // Additional food items
+  {
+    id: 5,
+    name: "Pizza",
+    imageUrl:
+      "https://images.pexels.com/photos/8609973/pexels-photo-8609973.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "Pizza is an Italian dish consisting of a yeasted flatbread typically topped with tomato sauce and cheese, and baked in an oven.",
+  },
+  {
+    id: 6,
+    name: "Taco",
+    imageUrl:
+      "https://img.freepik.com/free-photo/delicious-taco-plate_23-2150878143.jpg?size=626&ext=jpg&ga=GA1.1.984880611.1699715243&semt=sph",
+    description:
+      "Taco is a traditional Mexican dish composed of a tortilla filled with various ingredients, such as meat, cheese, and vegetables.",
+  },
+  {
+    id: 7,
+    name: "Hamburger",
+    imageUrl:
+      "https://images.pexels.com/photos/17642564/pexels-photo-17642564/free-photo-of-big-burger-on-a-wooden-cutting-board.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "A hamburger is a sandwich consisting of a cooked patty of ground meat, usually beef, placed inside a sliced bread roll or bun.",
+  },
+  {
+    id: 8,
+    name: "Sushi Roll",
+    imageUrl:
+      "https://img.freepik.com/free-photo/sushi-set-table_140725-5498.jpg?size=626&ext=jpg&ga=GA1.1.984880611.1699715243&semt=ais",
+    description:
+      "Sushi rolls are a variation of traditional sushi, where ingredients like fish, vegetables, and rice are wrapped in seaweed.",
+  },
+  {
+    id: 9,
+    name: "Spaghetti Bolognese",
+    imageUrl:
+      "https://img.freepik.com/free-photo/spaghetti-meatball-topped-with-parmesan-basil-food-photography_53876-96047.jpg?size=626&ext=jpg&ga=GA1.1.984880611.1699715243&semt=ais",
+    description:
+      "Spaghetti Bolognese is a classic Italian pasta dish with a meat-based sauce, typically served over spaghetti noodles.",
   },
 ];
