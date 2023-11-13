12bad185e9600b001e12b93f7feae532f946490b
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-13 11:14:53 +0530

    Configure tailwind to add custom styles given in styles-guide.md

--- react-practice-projects/junior-level/project-01/tailwind.config.js
+++ react-practice-projects/junior-level/project-01/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "325px",
+      },
+      colors: {
+        cyan: {
+          50: "hsl(0, 0%, 100%)",
+          100: "hsl(189, 41%, 97%)",
+          200: "hsl(185, 41%, 84%)",
+          300: "hsl(184, 14%, 56%)",
+          400: "hsl(186, 14%, 43%)",
+          500: "hsl(183, 100%, 15%)",
+          600: "hsl(172, 67%, 45%)",
+        },
+      },
+      fontFamily: {
+        display: "Space Mono, monospace",
+      },
+    },
   },
   plugins: [],
 };
