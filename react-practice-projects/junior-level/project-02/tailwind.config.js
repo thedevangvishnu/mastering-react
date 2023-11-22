c817d7ac676e80dd84d9f41fa1443c33da97e8d8
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-22 11:19:54 +0530

    Create custom sytles and configure them in tailwind.config.js

--- react-practice-projects/junior-level/project-02/tailwind.config.js
+++ react-practice-projects/junior-level/project-02/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "325px",
+      },
+      colors: {
+        gradient: {
+          100: "hsl(236, 72%, 79%)",
+          200: "hsl(237, 63%, 64%)",
+        },
+        grayishBlue: {
+          100: "hsl(240, 78%, 98%)",
+          200: "hsl(234, 14%, 74%)",
+          300: "hsl(233, 13%, 49%)",
+          400: "hsl(232, 13%, 33%)",
+        },
+      },
+      fontFamily: {
+        display: "Montserrat, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
